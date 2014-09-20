685ea27e5061630676667d9635ce8e875b954c87
Anthony <anthony.musco@stonybrook.edu>
2014-09-20 12:27:08 -0400

    Testing GameSurfaceView callback implementation.

--- app/src/main/java/com/redrock/unhackathon/GameSurfaceView.java
+++ app/src/main/java/com/redrock/unhackathon/GameSurfaceView.java
@@ -2,8 +2,9 @@ package com.redrock.unhackathon;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.util.ArrayList;
@@ -17,7 +18,7 @@ import java.util.ArrayList;
  * <dt><b>Date Created:</b></dd>
  *          9/20/2014
  */
-public class GameSurfaceView extends SurfaceView {
+public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
 
     // A different comment here.
     private Bitmap bmp;
@@ -27,13 +28,34 @@ public class GameSurfaceView extends SurfaceView {
     public GameSurfaceView(Context context) {
 
         super(context);
+        setWillNotDraw(false);
+        getHolder().addCallback(this);
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder arg0) {
+        Bitmap background = BitmapFactory.decodeResource(getResources(), R.drawable.test);
+        float scale = (float) background.getHeight() / (float) getHeight();
+        int newWidth = Math.round(background.getWidth() / scale);
+        int newHeight = Math.round(background.getHeight() / scale);
+        bmp = Bitmap.createScaledBitmap(background, newWidth, newHeight, true);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
 
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
 
-        canvas.drawColor(Color.BLACK);
+        canvas.drawBitmap(bmp, 0, 0, null);
         sprite.onDraw(canvas);
 
     }
